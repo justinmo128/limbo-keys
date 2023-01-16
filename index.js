837a53f81a29126c25690abed339bd6733eef455
justinmo128 <j.mo1@share.epsb.ca>
2023-01-16 11:22:53 -0700

    buh

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ let cnv = document.getElementById("canvas");
 let ctx = cnv.getContext("2d");
 cnv.width = 640;
 cnv.height = 480;
-
-let keyImg = document.getElementById("keyImg");
+const keyImg = document.getElementById("keyImg");
 let sequenceNum = 0;
+let keySpeed = 4;
+let loop = 0;
 class Key {
     constructor(x, y, pos) {
         this.x = x;
@@ -29,85 +30,83 @@ function draw() {
     for (let i = 0; i < 8; i++) {
         ctx.drawImage(keyImg, keys[i].x, keys[i].y)
     }
-    setTimeout(draw, 1000/60);
+    setTimeout(draw, 1000/240);
 }
 
-sequence();
-function sequence() {
-    if (sequenceNum === 0) {
-        diagonalSwap();
-    } else if (sequenceNum === 1) {
-        loop = 0;
-        bigRotation();
+for (let i = 0; i < 21; i++) {
+    setTimeout(choose, i * 250);
+}
+function choose() {
+    let randNum = Math.floor(Math.random() * 2)
+    if (randNum === 0 || randNum) {
+        const curInt = setInterval(diagonalSwap, 1000/120);
+        setTimeout(() => {
+            clearInterval(curInt);
+            for (let i = 0; i < 8; i++) {
+                if (keys[i].pos % 4 === 0) {
+                    keys[i].pos += 3;
+                } else if (keys[i].pos === 1 || keys[i].pos === 5) {
+                    keys[i].pos++;
+                } else if (keys[i].pos === 2 || keys[i].pos === 6) {
+                    keys[i].pos--;
+                } else {
+                    keys[i].pos -= 3;
+                }
+                keys[i].x = 235 + i % 2 * 100;
+                keys[i].y = 55 + Math.floor(i / 2) * 100;
+            }
+        }, 250);
+    } else {
+        const curInt = setInterval(bigRotation, 1000/120);
+        setTimeout(() => {
+            clearInterval(curInt);
+            for (let i = 0; i < 8; i++) {
+                if (keys[i].pos === 0) {
+                    keys[i].pos++;
+                } else if (keys[i].pos === 7) {
+                    keys[i].pos--;
+                } else if (keys[i].pos % 2) {
+                    keys[i].pos += 2;
+                } else {
+                    keys[i].pos -= 2;
+                }
+                keys[i].x = 235 + i % 2 * 100;
+                keys[i].y = 55 + Math.floor(i / 2) * 100;
+            }
+        }, 250);
     }
-    setTimeout(sequence, 1);
 }
 
 function diagonalSwap() {
-    if (keys.find(k => k.pos === 0).x > 234 && keys.find(k => k.pos === 0).x < 336) {
+    if (keys.find(k => k.pos === 0).x < 336) {
         for (let i = 0; i < 8; i++) {
             if (keys[i].pos % 2 === 0) {
-                keys[i].x++;
+                keys[i].x += keySpeed;
             } else {
-                keys[i].x--;
+                keys[i].x -= keySpeed;
             }
             if (Math.floor(keys[i].pos / 2) % 2 === 0) {
-                keys[i].y++;
+                keys[i].y += keySpeed;
             } else {
-                keys[i].y--;
+                keys[i].y -= keySpeed;
             }
         }
-    } else {
-        for (let i = 0; i < 8; i++) {
-            if (keys[i].x <= 235) {
-                keys[i].x = 235;
-            } else {
-                keys[i].x = 335;
-            }
-            if (keys[i].pos % 4 === 0) {
-                keys[i].pos += 3;
-            } else if (keys[i].pos === 1 || keys[i].pos === 5) {
-                keys[i].pos++;
-            } else if (keys[i].pos === 2 || keys[i].pos === 6) {
-                keys[i].pos--;
-            } else {
-                keys[i].pos -= 3;
-            }
-        }
-        sequenceNum++;
     }
 }
 
 function bigRotation() {
-    if (keys.find(k => k.pos === 0).x > 234 && keys.find(k => k.pos === 0).x < 336) {
+    if (keys.find(k => k.pos === 0).x < 336) {
         for (let i = 0; i < 8; i++) {
             if (keys[i].pos % 7 !== 0) {
                 if (keys[i].pos % 2) {
-                    keys[i].y++;
+                    keys[i].y += keySpeed;
                 } else {
-                    keys[i].y--;
-                }
+                    keys[i].y -= keySpeed;
+                } 
             } else if (keys[i].pos === 0) {
-                keys[i].x++;
+                keys[i].x += keySpeed;
             } else {
-                keys[i].x--;
-            }
-        }
-    } else {
-        for (let i = 0; i < 8; i++) {
-            if (keys[i].x <= 235) {
-                keys[i].x = 235;
-            } else {
-                keys[i].x = 335;
-            }
-            if (keys[i].pos === 0) {
-                keys[i].pos++;
-            } else if (keys[i].pos === 7) {
-                keys[i].pos--;
-            } else if (keys[i].pos % 2) {
-                keys[i].pos += 2;
-            } else {
-                keys[i].pos -= 2;
+                keys[i].x -= keySpeed;
             }
         }
     }
